1c51cd8a567dd7703578a1ebd73daab9e3513183
tochi27 <tochukwuamaechina@gmail.com>
2023-05-27 14:59:17 +0100

    first commit fot react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import{ Header } from './component/Header'
+import{ Navigations } from './component/Navigations'
+import { Footer } from './component/Footer'
+
+export const App = () => {
+  return (
+    <div>
+      <Header />
+      <Navigations />
+      <Footer />
+    </div>
+  )
+}
---
+++ src/component/Navigations.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Navigations = () => {
+  return (
+    <div>Navigations</div>
+  )
+}
